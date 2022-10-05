8b085615f250a66812588161d555f99800a7bf38
jsellars <flyingcable98@gmail.com>
2022-10-05 17:52:56 +1030

    Add functionality to update users (incomplete)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update]
+  before_action :correct_user, only: %i[edit update]
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -18,6 +24,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
@@ -28,5 +44,22 @@ class UsersController < ApplicationController
     # Strong parameters
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+
+  # Before filters
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in.'
+    redirect_to login_url, status: :see_other
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
   end
 end
