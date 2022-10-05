2e0b38c1da1d1aeaa38ff3290c39aa40f262ca52
jsellars <flyingcable98@gmail.com>
2022-10-05 14:57:16 +1030

    Implement advanced login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -66,7 +66,7 @@ end
 
 class LogoutTest < Logout
 
-  test 'successful lougout' do
+  test 'successful logout' do
     assert_not is_logged_in?
     assert_response :see_other
     assert_redirected_to root_url
@@ -77,5 +77,26 @@ class LogoutTest < Logout
     assert_select 'a[href=?]', login_path
     assert_select 'a[href=?]', logout_path, count: 0
     assert_select 'a[href=?]', user_path(@user), count: 0
+  end
+
+  test 'should still work after logout in second window' do
+    delete logout_path
+    assert_redirected_to root_url
+  end
+end
+
+class RememberingTest < UsersLogin
+
+  test 'login with remembering' do
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test 'login without remembering' do
+    # Log in to set the cookie.
+    log_in_as(@user, remember_me: '1')
+    # Log in again and verify that the cookie is deleted
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
   end
 end
